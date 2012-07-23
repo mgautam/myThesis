c4be21a01a6ad47efa91d40590ec299ede8b4e47
gautam <gautam.raw@gmail.com>
2012-07-24 06:18:33 +1000

    Bash based Compile Script

---
+++ Build/compile.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+rm *.o
+
+REL=..
+DIRS=$REL/*
+for DIR in $DIRS
+do
+    if [ -d "$DIR" ]
+    then
+	#rm $DIR/*.o
+	FILES=$DIR/*.cpp
+	for FILE in $FILES
+	do
+	    if [ -e "$FILE" ]
+	    then
+		echo "Compiling "$FILE"";
+		ccache g++ -c $FILE -I $REL -o $FILE".o"
+	        mv $FILE".o" .
+	    fi
+	done
+    fi
+done
