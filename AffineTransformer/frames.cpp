d4105be8d17f24b23fd7e09cbd1811cba6f3d483
gautam <gautam.raw@gmail.com>
2012-07-30 01:27:39 +1000

    Affine Transformer - backward projection. Nearest Neighbor Approximation. Better interpolations should be introduced.

--- AffineTransformer/frames.cpp
+++ AffineTransformer/frames.cpp
@@ -14,14 +14,14 @@ void createFrames (char *fileName, char *FramesFolder, int *translation, int num
 	GIMAGE* output = createImage(OUT_WIDTH, OUT_HEIGHT, input->numColors);
 
 	double theta,sintheta,costheta;
-	double rowcol[2];
-	double rowcolOut[2];
+	double rowIn,colIn;
+	double rowOut, colOut;
 	char filename[100];
 	for (int iter = 0; iter < numFrames; iter++) {
 		
-		for (rowcol[0] = 0; rowcol[0] < OUT_HEIGHT; rowcol[0]++)
-			for (rowcol[1] = 0; rowcol[1] < OUT_WIDTH; rowcol[1]++)
-				output->imageData[((int)rowcol[0])*OUT_WIDTH+(int)rowcol[1]] = 0.49; // 0.49 -> White Background; +-0.5-> Black Background
+		for (rowIn = 0; rowIn < OUT_HEIGHT; rowIn++)
+			for (colIn = 0; colIn < OUT_WIDTH; colIn++)
+				output->imageData[((int)rowIn)*OUT_WIDTH+(int)colIn] = 0.49; // 0.49 -> White Background; +-0.5-> Black Background
 
 
 		theta = 2*3.14*(double)iter/(double)numFrames;
@@ -29,15 +29,18 @@ void createFrames (char *fileName, char *FramesFolder, int *translation, int num
 		sintheta = sin(theta);
 		costheta = cos(theta);
 
-		for (rowcol[0] = -input->height/2; rowcol[0] < input->height/2; rowcol[0]++)
-			for (rowcol[1] = -input->width/2; rowcol[1] < input->width/2; rowcol[1]++) {
-				rowcolOut[0] = rowcol[0]*costheta + rowcol[1]*sintheta;
-				rowcolOut[1] = -rowcol[0]*sintheta + rowcol[1]*costheta;
-
-				if (rowcolOut[0]+translation[1] < OUT_HEIGHT/2 && rowcolOut[0]+translation[1] >= -OUT_HEIGHT/2)
-					if (rowcolOut[1]+translation[0] < OUT_WIDTH/2 && rowcolOut[1]+translation[0] >= -OUT_WIDTH/2)
-						output->imageData[((int)rowcolOut[0]+translation[1]+OUT_HEIGHT/2)*OUT_WIDTH+(int)rowcolOut[1]+translation[0]+OUT_WIDTH/2] 
-														= input->imageData[((int)rowcol[0]+input->height/2)*input->width+((int)rowcol[1]+input->width/2)];					
+		for (int y = -OUT_HEIGHT/2; y < OUT_HEIGHT/2; y++)
+			for (int x = -OUT_WIDTH/2; x < OUT_WIDTH/2; x++) {
+				rowOut = y - translation[1];
+				colOut = x - translation[0];
+				rowIn = rowOut*costheta - colOut * sintheta;
+				colIn = rowOut*sintheta + colOut * costheta;
+		
+		
+				if (rowIn < input->height/2 && rowIn >= -input->height/2)
+					if (colIn < input->width/2 && colIn >= -input->width/2)
+						output->imageData[((int)rowOut+OUT_HEIGHT/2)*OUT_WIDTH+(int)colOut+OUT_WIDTH/2] 
+														= input->imageData[((int)rowIn+input->height/2)*input->width+((int)colIn+input->width/2)];					
 			}
 
 		sprintf (filename,"%s/%d.bmp",FramesFolder,iter);
